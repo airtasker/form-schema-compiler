0441b43c4819257b857b02e0161bf5dab6f81dc0
Eric Chen <eric.chen@airtasker.com>
2018-04-24 14:19:59 +1000

    bump version and update compatible version

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,15 @@ import { COMPATIBLE_SCHEMA_VERSION } from "./const";
 export const hasKey = (obj, key) =>
   Object.prototype.hasOwnProperty.call(obj, key);
 
-export const isVersionCompatible = version =>
-  version >= COMPATIBLE_SCHEMA_VERSION[0] &&
-  version <= COMPATIBLE_SCHEMA_VERSION[1];
+const semvarToArray = version => version.split(".").map(Number);
+
+export const isVersionCompatible = version => {
+  const versions = semvarToArray(version);
+  if (versions.length !== 3) {
+    throw new Error(`can't recognize your schema version: ${version}`);
+  }
+
+  const [min, max] = COMPATIBLE_SCHEMA_VERSION.map(semvarToArray);
+
+  return versions.every((v, i) => v >= min[i] && v <= max[i]);
+};
